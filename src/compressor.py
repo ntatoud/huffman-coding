ee590dbc807ace29d1dd21566e5a1d425cf3cfab
Noe TATOUD <noe.tatoud@gmail.com>
2024-11-07 20:32:59 +0100

    feat(#7): Steps 1 to 3 of compression

--- src/compressor.py
+++ src/compressor.py
@@ -1,8 +1,10 @@
+from io import BufferedReader, BytesIO
 from typing import NamedTuple, Dict
-from io import BufferedReader
+
+from src.binary_code import BinaryCode, Bit
 from src.counter import Counter
 from src.huffman_tree import HuffmanTree
-from src.binary_code import BinaryCode, Bit
+from src.priority_queue import PriorityQueue
 
 
 class Statistics(NamedTuple):
@@ -10,18 +12,68 @@ class Statistics(NamedTuple):
     length: int
 
 
-def statistics(source: BufferedReader) -> Statistics:
+def statistics(source: BufferedReader | BytesIO) -> Statistics:
     """Compute the number of bytes in a binary stream and the number of occurences of each byte"""
     counter = Counter()
-    return Statistics(counter, length=len(counter.elements))
+    data = source.read()
+
+    for byte in data:
+        counter.increase(byte)
+
+    return Statistics(counter, len(data))
 
 
-def huffman_tree[T](stat: Counter[T]) -> HuffmanTree[T]:
+def huffman_tree[TData](stat: Counter[TData]) -> HuffmanTree[TData]:
     """Builds a HuffmanTree from a Counter"""
-    return HuffmanTree(list(stat.elements)[0], nb_occurrences=1)
+
+    tree_queue: PriorityQueue[HuffmanTree] = PriorityQueue()
+
+    # Build the Huffman leaf priority queue
+    for element in sorted(stat.elements):  # type: ignore
+        tree_queue.append(
+            HuffmanTree(
+                element=element, nb_occurrences=stat.get_nb_occurrences(element)
+            )
+        )
+
+    print(tree_queue)
+
+    while len(tree_queue) > 1:
+        first_tree = tree_queue.pop()
+        second_tree = tree_queue.pop()
+        tree_queue.append(HuffmanTree(left_child=first_tree, right_child=second_tree))
+
+    return tree_queue.pop()
 
 
-def binary_codes(stat: HuffmanTree) -> Dict[int, BinaryCode]:
+def binary_codes[TData](stats: HuffmanTree[TData]) -> Dict[TData, BinaryCode]:
     """Builds each byte's binary code"""
+    result = {}
 
-    return {0b01010101: BinaryCode(Bit.ZERO, Bit.ONE)}
+    binary_code = BinaryCode(Bit.ZERO)
+    return binary_codes_recursive(stats, result, binary_code)
+
+
+def binary_codes_recursive[TData](
+    stat: HuffmanTree[TData],
+    binary_codes_map: Dict[TData, BinaryCode],
+    current_binary_code: BinaryCode,
+) -> Dict[TData, BinaryCode]:
+    # Base case: if we reach a leaf node, add the element and its binary code to the map
+    if stat.is_leaf:
+        del current_binary_code[0]  # remove trailing 0 from tree root
+        binary_codes_map[stat.element] = current_binary_code  # type: ignore
+        return binary_codes_map
+
+    # Recursive case: traverse left and right subtrees
+    if stat.left_child:
+        left_code = BinaryCode(*current_binary_code.bits)
+        left_code.add(Bit.ZERO)
+        binary_codes_recursive(stat.left_child, binary_codes_map, left_code)
+
+    if stat.right_child:
+        right_code = BinaryCode(*current_binary_code.bits)
+        right_code.add(Bit.ONE)
+        binary_codes_recursive(stat.right_child, binary_codes_map, right_code)
+
+    return binary_codes_map
---
+++ tests/test_compressor.py
@@ -0,0 +1,79 @@
+import io
+# A 65, B 66, C 67, D 68, E 69, F 70, G 71
+import itertools
+from io import BytesIO
+
+import pytest
+
+from src.binary_code import BinaryCode, Bit
+from src.compressor import statistics, huffman_tree, binary_codes
+from src.counter import Counter
+from src.huffman_tree import HuffmanTree
+
+object_to_compress = b"BACFGABDDACEACGH"
+data_compressed = bytes(
+    [52, 50, 2]
+    + list(int.to_bytes(15, 4, byteorder="big"))
+    + list(
+        itertools.chain(
+            *[
+                list(int.to_bytes(object_to_compress.count(i), 4, byteorder="big"))
+                for i in range(256)
+            ]
+        )
+    )
+    + [145, 159, 105, 229, 100]
+)
+
+
+@pytest.fixture(scope="function")
+def data_stream() -> BytesIO:
+    return io.BytesIO(object_to_compress)
+
+
+def test_statistics(data_stream):
+    stat, nb = statistics(data_stream)
+    assert nb == 16
+    assert stat == Counter({65: 4, 66: 2, 67: 3, 68: 2, 69: 1, 70: 1, 71: 2, 72: 1})
+
+
+def test_arbre_huffman(data_stream):
+    stat, _ = statistics(data_stream)
+    huffman_tree_computed = huffman_tree(stat)
+    huffman_tree_expected = HuffmanTree(
+        left_child=HuffmanTree(
+            left_child=HuffmanTree(
+                left_child=HuffmanTree(72, 1), right_child=HuffmanTree(66, 2)
+            ),
+            right_child=HuffmanTree(65, 4),
+        ),
+        right_child=HuffmanTree(
+            left_child=HuffmanTree(
+                left_child=HuffmanTree(68, 2), right_child=HuffmanTree(71, 2)
+            ),
+            right_child=HuffmanTree(
+                left_child=HuffmanTree(
+                    left_child=HuffmanTree(69, 1), right_child=HuffmanTree(70, 1)
+                ),
+                right_child=HuffmanTree(67, 3),
+            ),
+        ),
+    )
+    assert huffman_tree_expected.equivalent(huffman_tree_computed)
+
+
+def test_binary_codes(data_stream):
+    stat, _ = statistics(data_stream)
+    arbre = huffman_tree(stat)
+    binary_codes_computed = binary_codes(arbre)
+    binary_codes_expected = {
+        65: BinaryCode(Bit.ZERO, Bit.ONE),
+        66: BinaryCode(Bit.ZERO, Bit.ZERO, Bit.ONE),
+        67: BinaryCode(Bit.ONE, Bit.ONE, Bit.ONE),
+        68: BinaryCode(Bit.ONE, Bit.ZERO, Bit.ZERO),
+        69: BinaryCode(Bit.ONE, Bit.ONE, Bit.ZERO, Bit.ZERO),
+        70: BinaryCode(Bit.ONE, Bit.ONE, Bit.ZERO, Bit.ONE),
+        71: BinaryCode(Bit.ONE, Bit.ZERO, Bit.ONE),
+        72: BinaryCode(Bit.ZERO, Bit.ZERO, Bit.ZERO),
+    }
+    assert binary_codes_computed == binary_codes_expected
