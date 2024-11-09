32b4af5b226ddbda297c52a1a648c4343829be66
Noe TATOUD <noe.tatoud@gmail.com>
2024-11-09 17:25:59 +0100

    fix(#7): Fix compressor package exports

--- src/compressor/__init__.py
+++ src/compressor/__init__.py
@@ -1,2 +1,4 @@
 from .data_utils import binary_codes, huffman_tree, statistics
-from .main import compress, compress_to_bytes
+from .main import compress
+
+__all__ = [compress, binary_codes, huffman_tree, statistics]
